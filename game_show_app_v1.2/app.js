c0adb186962e3b85ce1cd4a5427f0d56174cc547
Simon <simonpham1294@gmail.com>
2020-10-07 09:42:24 -0300

    new changes

--- game_show_app_v1.2/app.js
+++ game_show_app_v1.2/app.js
@@ -1,11 +1,46 @@
 const qwerty = document.getElementById('qwerty'); 
 const phrase = document.getElementById('phrase');
 const missed = 0; 
+const phrases = [
+    'Captain America', 
+    'Iron Man', 
+    'Doctor Strange', 
+    'Scarlet Witch', 
+    'Black Widow', 
+    'Spider Man', 
+    'Hulk', 
+    'Hawkeye', 
+    'Ant Man'
+]
 
+// game starting function 
 function startGame ()  {
-    const button = document.getElementsByClassName('btn__reset'); 
+    const button = document.getElementsByClassName('btn__reset')[0]; 
     const div = document.getElementById('overlay');
     button.addEventListener('click', () => {
         div.style.display = 'none'; 
     }); 
-}   
+};   
+startGame(); 
+
+// random phrase generator - return an array of characters
+function getRandomPhraseArray () {
+    const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+    const splitPhrase = randomPhrase.split(); 
+    return splitPhrase;    
+};
+
+getRandomPhraseArray ()
+
+function addPhraseToDisplay () {
+    
+    for (let i = 0; i < splitPhrase.length; i++) {
+        
+        const listItem = document.createElement('li');
+        splitPhrases[i].textContent = listItem.value; 
+        const ul = document.getElementById('phrase').querySelector('ul'); 
+        ul.appendChild(listItem);  
+    };
+};
+
+
